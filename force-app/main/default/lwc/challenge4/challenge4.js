c7cfe401b3ecf9bbcd6e42f90be00682dad89bb4
Patryk Barnas <patrykbarnas@yahoo.com>
2025-01-30 12:54:21 -0600

    search functionality functioning properly

--- force-app/main/default/lwc/challenge4/challenge4.js
+++ force-app/main/default/lwc/challenge4/challenge4.js
@@ -1,35 +1,47 @@
 import { LightningElement } from 'lwc';
+import acdc from '@salesforce/resourceUrl/acdc';
+import metallica from '@salesforce/resourceUrl/metallica';
+import queen from '@salesforce/resourceUrl/queen';
+import rhcp from '@salesforce/resourceUrl/rhcp';
 
 export default class Challenge4 extends LightningElement {
     bands =[
         {
             id: 1,
             name: 'Queen',
-            image:  'https://upload.wikimedia.org/wikipedia/en/6/67/Pink_Floyd_-_Dark_Side_of_the_Moon.png',
+            image:  queen,
             description: 'a super cool bad'
         },
         {
             id: 2,
             name:'Red Hot Chili Peppers',
-            image: 'img',
+            image: rhcp,
             description: 'this band was also cool'
         },
         {
             id: 3,
             name: 'Metallica',
-            image: 'img',
+            image: metallica,
             description: 'this band was cool'
         },
         {
             id: 4,
             name: 'AC/DC',
-            image: 'img',
+            image: acdc,
             description: 'something different'
         },
     ]
     selectedDesc = '';
-
+    filteredBands = this.bands;
     handleShowDesc(event){
         this.selectedDesc = event.detail;
+    }
+
+    handleSearch(e){
+        const searchTerm = e.target.value.toLowerCase();
+        this.filteredBands = this.bands.filter((band) =>
+            band.name.toLowerCase().includes(searchTerm)
+        );
+
     }
 }
