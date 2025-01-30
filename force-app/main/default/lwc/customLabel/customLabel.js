a0b381b02fc7e16cf74efe0a8bff0c77a7e15e7c
Patryk Barnas <patrykbarnas@yahoo.com>
2025-01-30 12:25:18 -0600

    custom labels with translations functioning

---
+++ force-app/main/default/lwc/customLabel/customLabel.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+import GREETING_LABEL from '@salesforce/label/c.Greeting';
+
+export default class CustomLabel extends LightningElement {
+
+    greeting = GREETING_LABEL;
+
+
+}
